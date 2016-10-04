2c7e1424754953106c541a7a40b0db891fd97a91
kseniyasauk <kseniya_sauk@epam.com>
2016-10-04 19:37:01 +0300

    add task6_2_lib and task6_2_tests

---
+++ Task6_2_Tests/TestsForMethod.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task6_2_lib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Task6_2_Tests
+{
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class TestsForMethod
+    {
+        IntParse obj;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            obj = new IntParse();
+        }
+
+        [TestCleanup]
+        public void TestCleanUp()
+        {
+            obj = null;
+        }
+        
+        [TestMethod]
+        public void TstCheckForPositiveNumber()
+        {
+            //("Enter a number between −2,147,483,648 and +2,147,483,647 (inclusive):")
+            int result = obj.ConvertToInteger("34");
+            Assert.AreEqual(34, result);
+        }
+
+        [TestMethod]
+        public void TstCheckForNegativeNumber()
+        {
+            //("Enter a number between −2,147,483,648 and +2,147,483,647 (inclusive):")
+            int result = obj.ConvertToInteger("-34");
+            Assert.AreEqual(-34, result);
+        }
+
+        [TestMethod]
+        public void TstCheckForRightBorder()
+        {
+            //("Enter a number between −2,147,483,648 and +2,147,483,647 (inclusive):")
+            int result = obj.ConvertToInteger("2147483647");
+            Assert.AreEqual(2147483647, result);
+        }
+        
+        [TestMethod]
+        public void TstCheckForLeftBorder()
+        {
+            //("Enter a number between −2,147,483,648 and +2,147,483,647 (inclusive):")
+            int result = obj.ConvertToInteger("−2147483648");
+            Assert.AreEqual(-2147483648, result);
+        }
+
+        [TestMethod]
+        public void TstCheckForLowerThenLeftBorder()
+        {
+            //("Enter a number between −2,147,483,648 and +2,147,483,647 (inclusive):")
+            int result = obj.ConvertToInteger("−2147483649");
+            Assert.AreEqual(-1, result);
+        }
+
+        [TestMethod]
+        public void TstCheckForHigherThenRightBorder()
+        {
+            //("Enter a number between −2,147,483,648 and +2,147,483,647 (inclusive):")
+            int result = obj.ConvertToInteger("2147483649");
+            Assert.AreEqual(-1, result);
+        }
+
+        [TestMethod]
+        public void TstCheckForEmpty()
+        {
+            //("Enter a number between −2,147,483,648 and +2,147,483,647 (inclusive):")
+            int result = obj.ConvertToInteger("");
+            Assert.AreEqual(-1, result);
+        }
+
+        [TestMethod]
+        public void TstCheckForString()
+        {
+            //("Enter a number between −2,147,483,648 and +2,147,483,647 (inclusive):")
+            int result = obj.ConvertToInteger("error!");
+            Assert.AreEqual(-1, result);
+        }
+    }
+}
---
+++ Task6_2_lib/IntParse.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Task6_2_lib
+{
+    public class IntParse
+    {
+        public int ConvertToInteger(string input)
+        {
+            int defaultValue = -1;
+            //string pattern = @"^\-?[0-9]+$";
+            //Regex rgx = new Regex(pattern);
+            //Match match = rgx.Match(input);
+            try
+            {
+                defaultValue = checked(Convert.ToInt32(input));
+                Console.WriteLine($"Number is: {defaultValue}");
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+            //Console.ReadKey();
+            return defaultValue;
+
+            
+        }
+    }
+}
